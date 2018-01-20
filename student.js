72faf1e7ed2065600e83b2b140a8b943fef76013
anandg112 <anandg112@gmail.com>
2018-01-19 21:42:15 -0500

    added more methods to Person superclass

--- student.js
+++ student.js
@@ -1,11 +1,16 @@
 class Person {
-     constructor(name, quirkyFact) {
+     constructor(name, quirkyFact, email) {
             this.name = name;
             this.quirkyFact = quirkyFact;
+            this.email = email;
        }
 
      bio() {
         return `My name is ${this.name} and here is my quirky fact: ${this.quirkyFact}`;
+     }
+
+     snooze() {
+        return `Time to sleep cuz ${this.quirkyFact}`;
      }
 }
 
@@ -25,10 +30,14 @@ class Mentor extends Person {
       }
 }
 
-let student1 = new Student("May", "I am cool!");
+let student1 = new Student("May", "I am cool!", "anandg112@gmail.com");
 student1.enroll("Feb 2017");
 console.log(student1);
+console.log(student1.bio());
+console.log(student1.snooze());
 
 let Mentor1 = new Mentor("Anand", "I am awesome!");
 Mentor1.goOnShift();
 console.log(Mentor1);
+console.log(Mentor1.bio());
+console.log(Mentor1.snooze());
