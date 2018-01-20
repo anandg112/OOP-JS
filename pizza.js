9dbcbf69813aac852ce36e1d5dd47de4e9573731
anandg112 <anandg112@gmail.com>
2018-01-20 14:24:59 -0500

    added getter and setter methods

--- pizza.js
+++ pizza.js
@@ -8,6 +8,22 @@ class Pizza {
       addTopping(topping) {
         this.toppings.push(topping);
       }
+
+      set size(size) {
+        if(size === 's' || size === 'm' || size === 'l') {
+           this.size = 5;
+       }
+      }
+
+      get size() {
+        return this.size;
+      }
+    
+      get price() {
+        const basePrice = 10;
+        const toppingPrice = 2;
+        return basePrice + this.toppings.length * toppingPrice;
+      }
 }
 
 let pizza = new Pizza('large', 'thin');
@@ -25,3 +41,7 @@ pizza2.addTopping("more cheese");
 console.log(pizza2.toppings);
 pizza2.addTopping("Pepperoni");
 console.log(pizza2.toppings);
+
+const pizza3 = new Pizza();
+pizza.price;
+pizza.size = 's'; //s
